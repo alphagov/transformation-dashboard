b746b388f542fe21650c14ce48ccb22ee27fc2bd
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2013-07-29 17:19:31 +0100

    Set up compile script

--- compile.sh
+++ compile.sh
@@ -4,22 +4,20 @@ set -e
 
 DESIGN_PRINCIPLES_ROOT="../design-principles"
 DIRECTORY="$DESIGN_PRINCIPLES_ROOT/public"
-GUIDANCE_PATH="$DIRECTORY/service-manual"
-SEARCH_CONTENT_PATH="$DESIGN_PRINCIPLES_ROOT/db/index/"
+GUIDANCE_PATH="$DIRECTORY/transformation"
 
 if [ ! -d "$DIRECTORY" ]; then
   echo "Couldn't find design principles app in $DIRECTORY"
 fi
 
 if [ -d $GUIDANCE_PATH ]; then
-  echo "Emptying existing service-manual folder"
+  echo "Emptying existing transformation folder"
   rm -rf $GUIDANCE_PATH
   rm -rf ./_site
 fi
 
 bundle exec jekyll ./_site
-cp -R ./_site/service-manual $GUIDANCE_PATH
-mv ./.search-index.json $SEARCH_CONTENT_PATH/service-manual.json
+cp -R ./_site/transformation $GUIDANCE_PATH
 
 echo "*****"
 echo "You should now use git to push the changes to the design principles app."
